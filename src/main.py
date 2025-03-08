1eb002e0a73166f60e087bc3bbf28618979500c7
Eline Snijder <149081642+elinesnijder@users.noreply.github.com>
2025-03-08 19:28:29 +0000

    voltei para o objetivo 1 porque o sympy nao esta a funcionar

--- src/main.py
+++ src/main.py
@@ -1,18 +1,16 @@
 import flet as ft
-from sympy import sympify, N
-
 
 class CalcButton(ft.ElevatedButton):
     def __init__(self, text, button_clicked, expand=2):
-        super().__init__(            
+        super().__init__(
             text=text,
             on_click=button_clicked,
             expand=expand,
             height=70,  
             width=70,   
-            style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10))) 
-        self.data = text 
-
+            style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10))
+        )
+        self.data = text
 
 class DigitButton(CalcButton):
     def __init__(self, text, button_clicked, expand=1):
@@ -20,42 +18,42 @@ class DigitButton(CalcButton):
         self.bgcolor = ft.colors.GREY
         self.color = ft.colors.WHITE
 
-
 class ActionButton(CalcButton):
     def __init__(self, text, button_clicked):
         CalcButton.__init__(self, text, button_clicked)
         self.bgcolor = ft.colors.TEAL_ACCENT_700
         self.color = ft.colors.WHITE
-
-
+        
 class ExtraActionButton(CalcButton):
     def __init__(self, text, button_clicked):
         CalcButton.__init__(self, text, button_clicked)
         self.bgcolor = ft.colors.BLUE_GREY_100
         self.color = ft.colors.BLACK
-
-
+        
 class CalculatorApp(ft.Container):
-    # application's root control (i.e. "view") containing all other controls
     def __init__(self):
         super().__init__()
         self.reset()
-        self.result = ft.Text(value="0", color=ft.colors.BLACK, size=75)
+
+        self.result = ft.Text(value="0", color=ft.colors.WHITE, size=40)  # Texto maior para melhor visualização
+        self.expand = True
         self.width = "100%"
         self.height = "100%"
         self.bgcolor = ft.colors.GREY_50
         self.border_radius = ft.border_radius.all(20)
         self.padding = 20
         self.alignment = ft.alignment.center
-        self.expression = ""
-        
         self.content = ft.Column(
             controls=[
                 ft.Row(controls=[self.result], alignment="end"),
                 ft.Row(
                     controls=[
-                        ExtraActionButton(text="AC", button_clicked=self.button_clicked),
-                        ExtraActionButton(text="+/-", button_clicked=self.button_clicked),
+                        ExtraActionButton(
+                            text="AC", button_clicked=self.button_clicked
+                        ),
+                        ExtraActionButton(
+                            text="+/-", button_clicked=self.button_clicked
+                        ),
                         ExtraActionButton(text="%", button_clicked=self.button_clicked),
                         ActionButton(text="/", button_clicked=self.button_clicked),
                     ]
@@ -86,7 +84,9 @@ class CalculatorApp(ft.Container):
                 ),
                 ft.Row(
                     controls=[
-                        DigitButton(text="0", expand=2, button_clicked=self.button_clicked),
+                        DigitButton(
+                            text="0", expand=2, button_clicked=self.button_clicked
+                        ),
                         DigitButton(text=".", button_clicked=self.button_clicked),
                         ActionButton(text="=", button_clicked=self.button_clicked),
                     ]
@@ -104,43 +104,68 @@ class CalculatorApp(ft.Container):
         elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
             if self.result.value == "0" or self.new_operand == True:
                 self.result.value = data
-                self.expression = data
                 self.new_operand = False
             else:
-                self.result.value += data
-                self.expression += data
+                self.result.value = self.result.value + data
 
-        elif data in ("+", "-", "*", "/", "(", ")"):
-            self.result.value += data
-            self.expression += data
-            self.new_operand = False
+        elif data in ("+", "-", "*", "/"):
+            self.result.value = self.calculate(
+                self.operand1, float(self.result.value), self.operator
+            )
+            self.operator = data
+            if self.result.value == "Error":
+                self.operand1 = "0"
+            else:
+                self.operand1 = float(self.result.value)
+            self.new_operand = True
 
-        elif data == "=":
-            try:
-                self.result.value = str(N(sympify(self.expression), 2))
-            except:
-                self.result.value = "Error"
+        elif data in ("="):
+            self.result.value = self.calculate(
+                self.operand1, float(self.result.value), self.operator
+            )
             self.reset()
 
-        elif data == "%":
-            try:
-                self.result.value = str(N(sympify(self.expression) / 100, 2))
-            except:
-                self.result.value = "Error"
+        elif data in ("%"):
+            self.result.value = float(self.result.value) / 100
             self.reset()
 
-        elif data == "+/-":
-            try:
-                self.result.value = str(N(-sympify(self.result.value), 2))
-                self.expression = self.result.value
-            except:
-                self.result.value = "Error"
+        elif data in ("+/-"):
+            if float(self.result.value) > 0:
+                self.result.value = "-" + str(self.result.value)
+
+            elif float(self.result.value) < 0:
+                self.result.value = str(
+                    self.format_number(abs(float(self.result.value)))
+                )
 
         self.update()
 
+    def format_number(self, num):
+        if num % 1 == 0:
+            return int(num)
+        else:
+            return num
+
+    def calculate(self, operand1, operand2, operator):
+
+        if operator == "+":
+            return self.format_number(operand1 + operand2)
+
+        elif operator == "-":
+            return self.format_number(operand1 - operand2)
+
+        elif operator == "*":
+            return self.format_number(operand1 * operand2)
+
+        elif operator == "/":
+            if operand2 == 0:
+                return "Error"
+            else:
+                return self.format_number(operand1 / operand2)
 
     def reset(self):
-        self.expression = ""
+        self.operator = "+"
+        self.operand1 = 0
         self.new_operand = True
 
 
