4e42a2e850efac47c9af03b07bbc41a07b2e62d7
Eline Snijder <149081642+elinesnijder@users.noreply.github.com>
2025-03-08 18:01:39 +0000

    tentativa de expressoes numericas com N()

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import flet as ft
+from sympy import sympify, N
 
 
 class CalcButton(ft.ElevatedButton):
@@ -46,17 +47,15 @@ class CalculatorApp(ft.Container):
         self.border_radius = ft.border_radius.all(20)
         self.padding = 20
         self.alignment = ft.alignment.center
+        self.expression = ""
+        
         self.content = ft.Column(
             controls=[
                 ft.Row(controls=[self.result], alignment="end"),
                 ft.Row(
                     controls=[
-                        ExtraActionButton(
-                            text="AC", button_clicked=self.button_clicked
-                        ),
-                        ExtraActionButton(
-                            text="+/-", button_clicked=self.button_clicked
-                        ),
+                        ExtraActionButton(text="AC", button_clicked=self.button_clicked),
+                        ExtraActionButton(text="+/-", button_clicked=self.button_clicked),
                         ExtraActionButton(text="%", button_clicked=self.button_clicked),
                         ActionButton(text="/", button_clicked=self.button_clicked),
                     ]
@@ -87,9 +86,7 @@ class CalculatorApp(ft.Container):
                 ),
                 ft.Row(
                     controls=[
-                        DigitButton(
-                            text="0", expand=2, button_clicked=self.button_clicked
-                        ),
+                        DigitButton(text="0", expand=2, button_clicked=self.button_clicked),
                         DigitButton(text=".", button_clicked=self.button_clicked),
                         ActionButton(text="=", button_clicked=self.button_clicked),
                     ]
@@ -107,68 +104,43 @@ class CalculatorApp(ft.Container):
         elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
             if self.result.value == "0" or self.new_operand == True:
                 self.result.value = data
+                self.expression = data
                 self.new_operand = False
             else:
-                self.result.value = self.result.value + data
+                self.result.value += data
+                self.expression += data
 
-        elif data in ("+", "-", "*", "/"):
-            self.result.value = self.calculate(
-                self.operand1, float(self.result.value), self.operator
-            )
-            self.operator = data
-            if self.result.value == "Error":
-                self.operand1 = "0"
-            else:
-                self.operand1 = float(self.result.value)
-            self.new_operand = True
+        elif data in ("+", "-", "*", "/", "(", ")"):
+            self.result.value += data
+            self.expression += data
+            self.new_operand = False
 
-        elif data in ("="):
-            self.result.value = self.calculate(
-                self.operand1, float(self.result.value), self.operator
-            )
+        elif data == "=":
+            try:
+                self.result.value = str(N(sympify(self.expression)))
+            except:
+                self.result.value = "Error"
             self.reset()
 
-        elif data in ("%"):
-            self.result.value = float(self.result.value) / 100
+        elif data == "%":
+            try:
+                self.result.value = str(N(sympify(self.expression) / 100))
+            except:
+                self.result.value = "Error"
             self.reset()
 
-        elif data in ("+/-"):
-            if float(self.result.value) > 0:
-                self.result.value = "-" + str(self.result.value)
-
-            elif float(self.result.value) < 0:
-                self.result.value = str(
-                    self.format_number(abs(float(self.result.value)))
-                )
+        elif data == "+/-":
+            try:
+                self.result.value = str(N(-sympify(self.result.value)))
+                self.expression = self.result.value
+            except:
+                self.result.value = "Error"
 
         self.update()
 
-    def format_number(self, num):
-        if num % 1 == 0:
-            return int(num)
-        else:
-            return num
-
-    def calculate(self, operand1, operand2, operator):
-
-        if operator == "+":
-            return self.format_number(operand1 + operand2)
-
-        elif operator == "-":
-            return self.format_number(operand1 - operand2)
-
-        elif operator == "*":
-            return self.format_number(operand1 * operand2)
-
-        elif operator == "/":
-            if operand2 == 0:
-                return "Error"
-            else:
-                return self.format_number(operand1 / operand2)
 
     def reset(self):
-        self.operator = "+"
-        self.operand1 = 0
+        self.expression = ""
         self.new_operand = True
 
 
