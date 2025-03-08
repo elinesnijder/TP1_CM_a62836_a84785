f1057a981a5d78ce04d56206c1327e36f02e2dca
Eline Snijder <149081642+elinesnijder@users.noreply.github.com>
2025-03-08 19:35:06 +0000

    expressões numericas com eval

--- src/main.py
+++ src/main.py
@@ -104,64 +104,40 @@ class CalculatorApp(ft.Container):
         elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
             if self.result.value == "0" or self.new_operand == True:
                 self.result.value = data
+                self.expression = data
                 self.new_operand = False
             else:
-                self.result.value = self.result.value + data
+                self.result.value += data
+                self.expression += data
 
-        elif data in ("+", "-", "*", "/"):
-            self.result.value = self.calculate(
-                self.operand1, float(self.result.value), self.operator
-            )
-            self.operator = data
-            if self.result.value == "Error":
-                self.operand1 = "0"
-            else:
-                self.operand1 = float(self.result.value)
-            self.new_operand = True
+        elif data in ("+", "-", "*", "/", "(", ")"):
+            self.result.value += data
+            self.expression += data
+            self.new_operand = False
 
         elif data in ("="):
-            self.result.value = self.calculate(
-                self.operand1, float(self.result.value), self.operator
-            )
+            try:
+                self.result.value = str(eval(self.expression))
+            except:
+                self.result.value = "Error"
             self.reset()
 
         elif data in ("%"):
-            self.result.value = float(self.result.value) / 100
+            try:
+                #calcular o valor percentual
+                self.result.value = str(eval(self.expression) / 100)
+            except:
+                self.result.value = "Error"
             self.reset()
 
         elif data in ("+/-"):
-            if float(self.result.value) > 0:
-                self.result.value = "-" + str(self.result.value)
-
-            elif float(self.result.value) < 0:
-                self.result.value = str(
-                    self.format_number(abs(float(self.result.value)))
-                )
-
+            try:
+                #altera o sinal do número
+                self.result.value = str(-float(self.result.value))
+                self.expression = self.result.value
+            except:
+                self.result.value = "Error"
         self.update()
-
-    def format_number(self, num):
-        if num % 1 == 0:
-            return int(num)
-        else:
-            return num
-
-    def calculate(self, operand1, operand2, operator):
-
-        if operator == "+":
-            return self.format_number(operand1 + operand2)
-
-        elif operator == "-":
-            return self.format_number(operand1 - operand2)
-
-        elif operator == "*":
-            return self.format_number(operand1 * operand2)
-
-        elif operator == "/":
-            if operand2 == 0:
-                return "Error"
-            else:
-                return self.format_number(operand1 / operand2)
 
     def reset(self):
         self.operator = "+"
