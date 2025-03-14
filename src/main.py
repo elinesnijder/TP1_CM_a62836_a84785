56bc135cafd4b9d6959742cf9ee313457a17cc3f
Eline Snijder <149081642+elinesnijder@users.noreply.github.com>
2025-03-14 15:57:55 +0000

    Tag Obj3

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import flet as ft
+import math
+import re
 
 class CalcButton(ft.ElevatedButton):
     def __init__(self, text, button_clicked, expand=2):
@@ -49,14 +51,36 @@ class CalculatorApp(ft.Container):
         self.expression = ""
         self.last_result = None
         
+        self.show_extra_buttons = False #botoes extras
+        
         self.content = ft.Column(
             controls=[
                 ft.Row(controls=[self.expression_text], alignment="end"),
                 ft.Row(controls=[self.result], alignment="end"),
+                #row de botoes extra
+                ft.Row(
+                    controls=[
+                        ActionButton(text="√", button_clicked=self.button_clicked),
+                        ActionButton(text="^", button_clicked=self.button_clicked),  
+                        ActionButton(text="log", button_clicked=self.button_clicked),  
+                        ActionButton(text="exp", button_clicked=self.button_clicked),
+                        ActionButton(text="!", button_clicked=self.button_clicked),
+                        ActionButton(text="|x|", button_clicked=self.button_clicked),
+                    ],
+                    visible=self.show_extra_buttons
+                ),
                 ft.Row(
                     controls=[
                         ExtraActionButton(text="AC", button_clicked=self.button_clicked),
-                        ExtraActionButton(text="+/-", button_clicked=self.button_clicked),
+                        ExtraActionButton(text="CE", button_clicked=self.button_clicked),  
+                        ExtraActionButton(text="←", button_clicked=self.button_clicked),  
+                        ExtraActionButton(text="↑", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        ExtraActionButton(text="(", button_clicked=self.button_clicked),  
+                        ExtraActionButton(text=")", button_clicked=self.button_clicked),  
                         ExtraActionButton(text="%", button_clicked=self.button_clicked),
                         ActionButton(text="/", button_clicked=self.button_clicked),
                     ]
@@ -102,31 +126,66 @@ class CalculatorApp(ft.Container):
             self.result.value = "0"
             self.reset()
 
+        elif data == "CE":  #apaga o que foi inserido
+            self.result.value = "0"
+        
+        elif data == "←":  
+            if self.result.value and self.result.value != "0":
+                self.result.value = self.result.value[:-1] #elimina ultimo caractere
+                self.expression = self.expression[:-1]  #elimina da expressão
+                if not self.result.value:
+                    self.result.value = "0"
+        
+        elif data == "↑": 
+            self.show_extra_buttons = not self.show_extra_buttons
+            self.content.controls[2].visible = self.show_extra_buttons
+            self.update()
+                    
         elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
             if self.last_result is not None:  # Se houver um resultado anterior
                 self.result.value = data  # Reinicia com o novo número
                 self.expression = data
                 self.last_result = None  
-            elif self.result.value == "0" or self.new_operand == True:
+            elif self.result.value == "0" or self.new_operand:
                 self.result.value = data
                 self.expression = data
                 self.new_operand = False
             else:
                 self.result.value += data
                 self.expression += data
+            self.result.value = self.format_number(self.expression)
 
         elif data in ("+", "-", "*", "/", "(", ")"):
-            if self.last_result is not None:  # Se houver um resultado antes
-                self.expression = self.result.value  # Continua do último resultado
+            #ultimo caractere é operando ou ()
+            if self.expression and self.expression[-1] in ("+", "-", "*", "/"):
+                #se for, não permite adicionar mais
+                #muda de operando se clicar noutro
+                if data in ("+", "-", "*", "/"):
+                    self.expression = self.expression[:-1] + data
+                    self.result.value = self.format_number(self.expression)
+                    self.update_expression_display()
+                return
+            
+            # último caractere é um parêntese de abertura
+            if self.expression and self.expression[-1] == "(" and data == "(":
+                return  #nao adiciona parênteses de abertura consecutivos
+
+            # último caractere é um parêntese de fechamento
+            if self.expression and self.expression[-1] == ")" and data == ")":
+                return  #nao adiciona parênteses de fechamento consecutivos
+            
+            if self.last_result is not None:
+                self.expression = self.result.value.replace(" ", "")
                 self.last_result = None 
             self.result.value += data
             self.expression += data
+            self.result.value = self.format_number(self.expression)
             self.new_operand = False
-
+            
         elif data == "=":
             try:
-                result = eval(self.expression)
-                self.result.value = str(round(result, 2))
+                result = eval(self.expression.replace(" ", ""))
+                self.result.value = self.format_number(str(round(result, 2)))
                 self.last_result = result
             except:
                 self.result.value = "Error"
@@ -135,24 +194,87 @@ class CalculatorApp(ft.Container):
 
         elif data == "%":
             try:
-                result = eval(self.expression) / 100
-                self.result.value = str(round(result, 2))
+                result = eval(self.expression.replace(" ", "")) / 100
+                self.result.value = self.format_number(str(round(result, 2)))
             except:
                 self.result.value = "Error"
             self.update_expression_display()
 
         elif data == "+/-":
             try:
-                result = -float(self.result.value)
-                self.result.value = str(round(result, 2)) #arredondamento
+                result = -float(self.result.value.replace(" ", ""))
+                self.result.value = self.format_number(str(round(result, 2))) #arredondamento
                 self.expression = self.result.value
+            except:
+                self.result.value = "Error"
+                
+        elif data == "√":
+            try:
+                result = math.sqrt(float(self.result.value.replace(" ", "")))
+                self.result.value = self.format_number(str(round(result, 2)))
+            except:
+                self.result.value = "Error"
+        
+        elif data == "^":
+            self.expression += "^"
+            self.result.value = self.format_number(self.expression)
+            
+        elif data == "log":
+            try:
+                result = math.log10(float(self.result.value.replace(" ", "")))
+                self.result.value = self.format_number(str(round(result, 2)))
+            except:
+                self.result.value = "Error"
+        
+        elif data == "exp":
+            try:
+                result = math.exp(float(self.result.value.replace(" ", "")))
+                self.result.value = self.format_number(str(round(result, 2)))
+            except:
+                self.result.value = "Error"
+                
+        elif data == "!":
+            try:
+                num = int(self.result.value.replace(" ", ""))
+                result = math.factorial(num)
+                self.result.value = self.format_number(str(result))
+            except:
+                self.result.value = "Error"
+                
+        elif data == "|x|":
+            try:
+                result = abs(float(self.result.value.replace(" ", "")))
+                self.result.value = self.format_number(str(round(result, 2)))
             except:
                 self.result.value = "Error"
 
         self.update_expression_display()
+        
+
+
+    def format_number(self, number_str):
+        try:
+            #remover espaços para nao haver erro
+            number_str = number_str.replace(" ", "")
+
+            #encontrar os numeros na expressao
+            def format_match(match):
+                num = match.group()
+                if "." in num:  #numeros deciamsi
+                    int_part, decimal_part = num.split(".")
+                    formatted_int = f"{int(int_part):,}".replace(",", " ")
+                    return f"{formatted_int}.{decimal_part}"
+                else:  #numeros inteiros
+                    return f"{int(num):,}".replace(",", " ")
+
+            #formatação nos numeors todos
+            formatted_expression = re.sub(r"-?\d+\.?\d*", format_match, number_str)
+            return formatted_expression
+        except ValueError:
+            return number_str
 
     def update_expression_display(self):
-        self.expression_text.value = self.expression
+        self.expression_text.value = self.format_number(self.expression)
         self.update()
 
     def reset(self):
